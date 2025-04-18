5099374c4e8a8e3ca0a08c5bbfdca855dbbe7f3c
AnjaliRawatAR <anjalivipulrawat@gmail.com>
2025-04-18 12:19:17 +0530

    third commit

--- Average Calculator/src/utils/client.js
+++ Average Calculator/src/utils/client.js
@@ -4,7 +4,7 @@ const API_URLS = {
   p: "http://20.244.56.144/evaluation-service/primes",
   f: "http://20.244.56.144/evaluation-service/fibo",
   e: "http://20.244.56.144/evaluation-service/even",
-  r: "http://20.244.56.144/evaluation-service/random",
+  r: "http://20.244.56.144/evaluation-service/rand",
 };
 
 exports.fetchNumbersFromAPI = async (numberId) => {
